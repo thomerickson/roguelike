2f481f46f41f4feb4a42f8268299811185b50f04
Thom Erickson <thomerickson@gmail.com>
2018-06-03 23:46:18 -0700

    Added new functionality: Entity rendering, map utilities (drawing of ground and walls), as well as player testing for walls

---
+++ entity.py
@@ -0,0 +1,17 @@
+# entity.py
+
+class Entity():
+    """
+    A generic object to represent players, enemies, items, whatever...
+    """
+
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+    
+    def move(self, dx, dy):
+        # move the entity by a given amount
+        self.x += dx
+        self.y += dy
